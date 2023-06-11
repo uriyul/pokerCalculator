269c4ef66ce95f26b75826adc956a9fc189795a0
Uri <uri@yulevich.org>
2023-06-11 14:33:13 +0300

    Fixed tests

--- odds_test.go
+++ odds_test.go
@@ -6,38 +6,21 @@ import (
 )
 
 func Test_odds(t *testing.T) {
-	// h1 := []card{
-	// 	{suit: Spades, value: 12},
-	// 	{suit: Spades, value: 13},
-	// }
-
-	// h2 := []card{
-	// 	{suit: Hearts, value: 14},
-	// 	{suit: Spades, value: 14},
-	// }
-
-	// board := []card{
-	// 	{suit: Hearts, value: 7},
-	// 	{suit: spades, value: 8},
-	// 	{suit: Spades, value: 9},
-	// 	// {suit: Spades, value: 5},
-	// }
-
 	h1 := []card{
-		{suit: Clubs, value: 3},
-		{suit: Spades, value: 4},
+		{suit: Spades, value: 12},
+		{suit: Spades, value: 13},
 	}
 
 	h2 := []card{
-		{suit: Hearts, value: 9},
-		{suit: Diamonds, value: 10},
+		{suit: Hearts, value: 14},
+		{suit: Spades, value: 14},
 	}
 
 	board := []card{
-		//{suit: Hearts, value: 7},
-		//{suit: Spades, value: 8},
-		//{suit: Spades, value: 9},
-		// {suit: "Spades", value: 5},
+		// {suit: Hearts, value: 7},
+		// {suit: Spades, value: 8},
+		// {suit: Spades, value: 9},
+		// {suit: Spades, value: 5},
 	}
 
 	handsData := HandsData{
@@ -45,7 +28,31 @@ func Test_odds(t *testing.T) {
 		board:     board,
 	}
 
+	// The expected values are the number of hands that have a given strength
+	expexted_h1 := []int{294394, 733544, 392909, 78023, 73340, 94935, 41716, 2420, 1019, 4}
+	expexted_h2 := []int{0, 613226, 674008, 209450, 20626, 29255, 149956, 15620, 29, 134}
+
 	o1, o2, tie := odds(&handsData)
 
 	fmt.Println(o1, o2, tie)
+
+	handsCount := 0
+	for _, v := range handsData.handsOdds[0].mapOdds {
+		handsCount += v
+	}
+
+	fmt.Println("		First hand     Second hand")
+	fmt.Println("_______________________________________")
+	for k := len(handsData.handsOdds[0].mapOdds); k > 0; k-- {
+		fmt.Printf("%v: %.02f\t\t%.02f %v %v\n", strength2String[strength(k)], float64(handsData.handsOdds[0].mapOdds[strength(k)])*100/float64(handsCount), float64(handsData.handsOdds[1].mapOdds[strength(k)])*100/float64(handsCount),
+			handsData.handsOdds[0].mapOdds[strength(k)], handsData.handsOdds[1].mapOdds[strength(k)])
+
+		if handsData.handsOdds[0].mapOdds[strength(k)] != expexted_h1[k-1] {
+			t.Errorf("Expected %v, got %v", expexted_h1[k-1], handsData.handsOdds[0].mapOdds[strength(k)])
+		}
+		if handsData.handsOdds[1].mapOdds[strength(k)] != expexted_h2[k-1] {
+			t.Errorf("Expected %v, got %v", expexted_h2[k-1], handsData.handsOdds[1].mapOdds[strength(k)])
+		}
+	}
+
 }
